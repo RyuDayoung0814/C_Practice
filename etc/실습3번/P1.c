3ffcfa5893dceb0885c9b3a4805c439764ee2085
RyuDayoung0814 <2019253089@yonsei.ac.kr>
2024-03-25 20:26:30 +0900

    feat: p1

---
+++ etc/실습3번/P1.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int dan;
+
+    scanf("%d", &dan);
+    
+    printf("%d x %d = %d", dan, 1, dan * 1);
+    printf("%d x %d = %d", dan, 2, dan * 2);
+    printf("%d x %d = %d", dan, 3, dan * 3);
+    printf("%d x %d = %d", dan, 4, dan * 4);
+    printf("%d x %d = %d", dan, 5, dan * 5);
+    printf("%d x %d = %d", dan, 6, dan * 6);
+    printf("%d x %d = %d", dan, 7, dan * 7);
+    printf("%d x %d = %d", dan, 8, dan * 8);
+    printf("%d x %d = %d", dan, 9, dan * 9);
+
+    return 0;
+}
